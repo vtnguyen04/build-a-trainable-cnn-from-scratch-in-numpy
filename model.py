1c37398edc9f382af6412b3305dfaf98c28cd4b7
Võ Thành Nguyễn <64852371+vtnguyen04@users.noreply.github.com>
2026-09-16 00:30:07 +0700

    Step 14: output_spatial_size

--- model.py
+++ model.py
@@ -105,8 +105,11 @@ def pad_2d(images, pad):
 
     return np.pad(images, pad_width, mode="constant", constant_values=0)
 
-# Step 14 - output_spatial_size (not yet solved)
-# TODO: implement
+# Step 14 - output_spatial_size
+def output_spatial_size(input_size, kernel, stride, padding):
+    # TODO: return the conv/pool output spatial dimension from input_size, kernel, stride, padding
+    
+    return (input_size + 2 * padding - kernel) // stride + 1
 
 # Step 15 - im2col (not yet solved)
 # TODO: implement
