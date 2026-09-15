c5fef0340f72b4ab0d907795d2f25470a10b3fb6
Võ Thành Nguyễn <64852371+vtnguyen04@users.noreply.github.com>
2026-09-16 00:22:13 +0700

    Step 10: he_std

--- model.py
+++ model.py
@@ -73,8 +73,10 @@ def accuracy(logits_or_probs, labels):
 
     return np.sum(max_probs == labels) / len(labels)
 
-# Step 10 - he_std (not yet solved)
-# TODO: implement
+# Step 10 - he_std
+def he_std(fan_in):
+    # TODO: return the He initialization standard deviation sqrt(2 / fan_in).
+    return np.sqrt(2 / fan_in)
 
 # Step 11 - he_init (not yet solved)
 # TODO: implement
